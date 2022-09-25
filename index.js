70d5104069d47b806490dcfc378266b52593b789
Rohit <rohit.singh11299@gmail.com>
2022-09-25 15:13:32 +0530

    new functionalities added

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ let todo_list = document.querySelector('.todo-list');
 let form = document.querySelector('form');
 let type_todo = document.getElementById('type-todo');
 let items_left = document.querySelector('.items-left');
-let check_Img = document.querySelector('.check');
 let selection = document.querySelector('.selection');
 let container = document.querySelector('.container');
 let theme_image = document.getElementById('theme-image');
@@ -10,9 +9,58 @@ let body = document.querySelector('body');
 let todo_fill = document.querySelector('.todo-fill');
 let text = document.querySelector('.text');
 let close = document.querySelector('.close')
+let check_Icon = document.querySelector('.check-icon');
+let check_Img = document.querySelector('.check');
+let active = document.getElementsByClassName('active')[0];
+
+function show_active(){
+    let active_task = (text.getElementsByClassName('checkNPara'))
+    let active_arr = Array.from(active_task);
+    // console.log(arr)
+    active_arr.forEach(element => {
+        // console.log(element.parentElement.parentElement)
+        if(element.children[0].classList.contains('active-check')){
+            element.parentElement.parentElement.classList.toggle ('display-active')
+        }
+    })
+}
+function show_completed(){
+    let completed_task = text.getElementsByClassName('checkNPara')
+    let completed_arr = Array.from(completed_task)
+    completed_arr.forEach(element => {
+        // console.log(element)
+        if(!element.children[0].classList.contains('active-check')){
+            element.parentElement.parentElement.classList.toggle('display-completed')
+        }
+    })
+}
+function clear_completed(){
+    let clear_completed = text.getElementsByClassName('checkNPara')
+    let clear_completed_arr = Array.from(clear_completed)
+    clear_completed_arr.forEach(element => {
+        // console.log(element)
+        if(element.children[0].classList.contains('active-check')){
+            element.parentElement.parentElement.remove();
+        }
+    })
+}
+selection.addEventListener('click',(e) => {
+    // console.log(e.target.classList);
+    if(e.target.classList.contains('active')){
+        console.log(e.target.classList)
+        show_active()
+    }
+    if(e.target.classList.contains('completed')){
+        console.log(e.target.classList)
+        show_completed()
+    }
+    if(e.target.classList.contains('clear-completed')){
+        console.log(e.target.classList)
+        clear_completed()
+    }
+})
 
 function toggleTheme(){
-    
     if(theme_image.getAttribute('src') == './Images/icon-sun.svg'){
         theme_image.setAttribute('src','./Images/icon-moon.svg')
         container.style.background = `url('./Images/bg-desktop-light.jpg') no-repeat center / cover`
@@ -27,7 +75,6 @@ function toggleTheme(){
     type_todo.classList.toggle('input-light-theme');
     text.classList.toggle("todo-list-bg");
 }
-
 function appendToList() {
     let textVal = type_todo.value;
     let addDiv = document.createElement('div');
@@ -35,7 +82,7 @@ function appendToList() {
     addDiv.innerHTML = `
     <div class="todo-list">
     <div class = checkNPara>
-    <div class="check"><img src="./Images/icon-check.svg" alt=""></div>
+    <div class="check"><img class = "check-icon" src="./Images/icon-check.svg" alt=""></div>
     <p>${textVal}</p>
     </div>
     <img class="close" src="./Images/icon-cross.svg" alt="">
@@ -44,37 +91,51 @@ function appendToList() {
 }
 
 function countItems(){
-    let items =  text.childElementCount;
-    // console.log(items)
-    if(items > 1){
-        items_left.textContent = `${items} tasks left`
+   let checked_length = document.getElementsByClassName('check-icon-style').length
+   let total_length = document.getElementsByClassName("main-class").length;
+   let length = total_length - checked_length;
+   if(length == 0){
+    items_left.textContent = "No Tasks Added"
+   }
+    if(length > 1){
+        items_left.textContent = `${length} Tasks Left`
     }
-    else if (items == 1){
-        items_left.textContent = `${items} task left`
+    else if (length == 1){
+        items_left.textContent = `${length} Task Left`
     }
-    else{
-        items_left.textContent = "no tasks added"
+    else if(checked_length == total_length && checked_length !== 0){
+        items_left.textContent = "All Tasks Completed"
     }
 }
-
-theme_image.addEventListener('click',toggleTheme)
-function check(){
-    let check_Icon = document.querySelector('.check-icon');
-    check_Icon.style.display = "block";
-    check_Img.style.background ="linear-gradient(129deg, hsl(192, 100%, 67%), hsl(280, 87%, 65%))";
-    check_Img.style.border = 'none';
+function check(e){
+    if(e.target.classList.contains('check-icon')){
+        e.path[1].classList.toggle('active-check');
+        e.path[1].lastElementChild.classList.toggle('check-icon-style');
+            e.path[2].lastElementChild.classList.toggle('line-thru')
+    }
+    else{
+        e.path[0].lastElementChild.classList.toggle('check-icon-style');
+        e.path[0].classList.toggle('active-check');
+        e.path[1].lastElementChild.classList.toggle('line-thru')
+    }
+    // console.log(e.target.classList)
 }
 function removeItems(e){
     e.remove();
 }
+
+theme_image.addEventListener('click',toggleTheme)
+
 text.addEventListener('click', (e) => {
-    console.log(e)
     if(e.target.classList.contains('close')){
         removeItems(e.path[2]);
     }
+    if(e.target.classList.contains('check-icon') || (e.target.classList.contains('check'))){
+        // console.log(e);
+        check(e)
+    }
     countItems();
 })
-
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
